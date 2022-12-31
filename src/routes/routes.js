2b91a626c8725dc2ab3ba5a9bfe1b3ebd7170a6b
Devendra kushwaha <dev297146@gmail.com>
2022-12-31 23:38:48 +0530

    Feature/issues/27 (#28)
    * fixed screen hide issues on typing (#26)
    
    * promotion screen

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home, LogIn, SignUp} from '../screens';
+import { Branding, Home, LogIn, SignUp } from '../screens';
 
 const Stack = createNativeStackNavigator();
 
 const Routes = ()=> {
   return(
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="Branding">
           <Stack.Screen options={{headerShown: false}} name="Login" component={LogIn} />
-          <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
+          <Stack.Screen options={{headerShown: false}} name="Branding" component={Branding} />
+          <Stack.Screen options={{headerShown: true}} name="Home" component={Home} />
           <Stack.Screen options={{headerShown: false}} name="SignUp" component={SignUp} />
         </Stack.Navigator>
     </NavigationContainer>
